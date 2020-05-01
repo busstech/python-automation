65859b37ee8837ae96e3215eb240f4521c3b964b
Warren Le Roux <warren@busstech.com>
2020-05-01 19:01:05 +0200

    Updated functions and added variables

--- Functions/ReadFile.py
+++ Functions/ReadFile.py
@@ -1,10 +1,31 @@
 # Function - Open a file
-inputFile = open('../Files/Students.txt', 'r')  # Specify the file to read
+# 1.    Standard reading of a file
+# inputFile = open('../Files/Students.txt', 'r')  # Specify the file to read
+# print(inputFile.read())                         # Print each row as it's read
+# inputFile.close()                               # Close the file being read
 
-count = 0                                       # count each line and start at id 0 (or first line)
+# 2.    Reading of a file and adding the line numbers before the row
+# inputFile = open('../Files/Students.txt', 'r')  # Specify the file to read
+# count = 0                                       # count each line and start at id 0 (or first line)
+# for line in inputFile:                          # iterate over every line in the file
+#     print(str(count) + line)                    # print line number and record
+#     count = count + 1                           # for every record increment by 1
+# inputFile.close()                               # close the file
 
-for line in inputFile:                          # iterate over every line in the file
-    print(str(count) + line)                    # print line number and record
-    count = count + 1                           # for every record increment by 1
+# 3.    Print the line count for each row in the file
+# inputFile = open('../Files/Students.txt', 'r')  # Specify the file to read
+# count = 0                                       # count each line and start at id 0 (or first line)
+# for line in inputFile:                          # iterate over every line in the file
+#     count = count + 1                           # for every record increment by 1
+#     print(count)                              # print line number and record
+# inputFile.close()                               # close the file
 
-inputFile.close()                               # close the file
+# 4.    Inspect a file
+import inspect
+import os
+
+f = inspect.currentframe()
+i = inspect.getframeinfo(f)
+print(i.lineno)
+print(i.filename)
+print(i.function)
---
+++ Variables/Variable.py
@@ -0,0 +1,6 @@
+# Variables and a short story
+
+# There once was a man named John, he was 70 years old.
+# He really liked the name John, but didn't like being 70 years old.'
+
+print("There once was a man named " + )
